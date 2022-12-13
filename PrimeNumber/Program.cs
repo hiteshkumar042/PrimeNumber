b8f989d0c038ce8ef2b08302772dbf76a6e219e7
hiteshkumar794 <hiteshkumar794@gmail.com>
2022-12-13 17:21:38 +0530

    Prime Number Program added

--- PrimeNumber/Program.cs
+++ PrimeNumber/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.ExceptionServices;
+﻿using System.ComponentModel;
+using System.Runtime.ExceptionServices;
 using System.Security.Cryptography.X509Certificates;
 
 namespace PrimeNumber
@@ -7,33 +8,9 @@ namespace PrimeNumber
     {
         static void Main(string[] args)
         {
-            int flag = 0;
-            Console.WriteLine("Enter a number to check prime: ");
-            int num = Convert.ToInt32(Console.ReadLine());
-           //0 and 1 are not a prime Numbers
-            if (num==0 || num == 1)
-            {
-                flag = 1;// flagged 1 for non prime
-            }
-            //to check num is divisible by any numbers from 2 to num/2
-            for (int i = 2; i <= num / 2; i++)
-            {
-                if (num % i == 0)
-                {
-                    //if is divisible by any number from 2 to num/2, flagged 1 for non prime
-                    flag = 1;
-                    break;//once it divisible any any one number no need to check for others
-                }
-            }
-            if (flag == 0)//flagged 0 for prime numbers
-            {
-                Console.WriteLine("It is PrimeNumber");
-            }
-            else
-            {
-                Console.WriteLine("It is not a PrimeNumber");
-
-            }       
+            //Object creation to access the primeCheckFn method 
+            PrimeNum obj= new PrimeNum();
+            obj.PrimeCheckFn();       
 
         }       
 
